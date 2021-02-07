39011e9ee5dec652b63044ba8c9d960dd1665302
Manu <3916435+m3nu@users.noreply.github.com>
2021-02-07 20:48:49 +0800

    Add examples on using repo filters

--- borgbase_api_client/queries.py
+++ borgbase_api_client/queries.py
@@ -11,6 +11,32 @@ query repoList {
 }
 """
 
+FILTER_REPOS_BY_NAME = """
+{
+  repoList(name: "web20") {
+    id
+    name
+    quota
+    quotaEnabled
+    lastModified
+    currentUsage
+  }
+}
+"""
+
+SINGLE_REPO_BY_ID = """
+query {
+  repo(repoId: "xxx999") {
+    id
+    name
+    quota
+    quotaEnabled
+    lastModified
+    currentUsage
+  }
+}
+"""
+
 KEY_DETAILS = """
 query repoList {
   sshList {
